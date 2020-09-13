e7af9e1273324e0fd2e31f9902ab59f6f8dc821a
Rich Sturim <rich.sturim@coxautoinc.com>
2020-09-13 17:26:05 -0400

    add back contact table

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 import CounterView from "./views/CounterView";
 
+import ContactView from "./views/ContactView";
 function App() {
     return (
         <Container>
             <h1>React without Redux</h1>
             <CounterView />
+            <ContactView />
         </Container>
     );
 }
