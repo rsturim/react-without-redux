334c6bc397c92843aa275ef68df6eef580588590
Rich Sturim <rich.sturim@coxautoinc.com>
2020-09-14 08:44:25 -0400

    remove index.css, fomantic css, and use semantic-css instead

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-import "fomantic-ui-css/semantic.min.css";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
